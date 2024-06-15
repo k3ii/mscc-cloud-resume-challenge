2be054da6d683525972d4ecddb0545e71264a337
k3ii <jainr@ringier.co.za>
2024-06-15 16:02:47 +0400

    fix: variable names

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -7,9 +7,9 @@ data "archive_file" "update_counter" {
 resource "aws_lambda_function" "update_counter" {
   filename         = data.archive_file.update_counter.output_path
   source_code_hash = data.archive_file.update_counter.output_base64sha256
-  function_name    = var.update_counter_function_name
+  function_name    = var.function_name
   role             = aws_iam_role.cloudresume_api_role.arn
-  handler          = var.update_counter_handler
+  handler          = var.function_handler
   runtime          = var.python_runtime
   timeout          = 10
   memory_size      = 128
