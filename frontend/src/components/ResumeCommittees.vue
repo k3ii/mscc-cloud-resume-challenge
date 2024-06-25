54a649880cec4152ee997368211f87ad016506d6
dereck lhw <dereck.lamhonwah@gmail.com>
2024-06-25 09:02:52 +0400

    style: tables

--- frontend/src/components/ResumeCommittees.vue
+++ frontend/src/components/ResumeCommittees.vue
@@ -3,24 +3,31 @@
 <template>
   <div>
     <h2 class="text-3xl font-bold">Committees</h2>
-    <div class="flex-grow border-b-2 border-gray-500"></div>
-    <table class="table-auto border-separate border-spacing-x-3">
+    <div class="flex-grow border-b-2 border-gray-500 mb-4"></div>
+    <table class="table-auto w-full border-collapse">
       <tbody>
         <tr>
-          <td>2016</td>
-          <td>Problem Writer</td>
-          <td>2016 CODEGATE Hacking Competition World Final</td>
-          <td>Seoul, S.Korea</td>
+          <td class="text-left">2016</td>
+          <td class="text-left font-bold">Problem Writer</td>
+          <td class="text-left">2016 CODEGATE Hacking Competition World Final</td>
+          <td class="text-right italic text-red-600">Seoul, S.Korea</td>
         </tr>
         <tr>
-          <td>2013</td>
-          <td>Organizer & Co-director</td>
-          <td>1st POSTECH Hackathon</td>
-          <td>Seoul, S.Korea</td>
+          <td class="text-left">2013</td>
+          <td class="text-left font-bold">Organizer & Co-director</td>
+          <td class="text-left">1st POSTECH Hackathon</td>
+          <td class="text-right italic text-red-600">Seoul, S.Korea</td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+table {
+  border-spacing: 0 8px; /* Add vertical spacing between rows */
+}
+td {
+  border-spacing: 8px; /* Add horizontal spacing between columns */
+}
+</style>
--- frontend/src/components/ResumeHonors.vue
+++ frontend/src/components/ResumeHonors.vue
@@ -7,50 +7,50 @@
     <div class="space-y-3">
       <div>
         <h3 class="text-xl uppercase">International</h3>
-        <table class="table-auto border-separate border-spacing-x-3">
+        <table class="table-auto w-full border-collapse">
           <tbody>
             <tr>
-              <td>2018</td>
-              <td>Finalist</td>
-              <td>DEFCON 26th CTF Hacking Competition World Final</td>
-              <td>Las Vegas, U.S.A</td>
+              <td class="text-left">2018</td>
+              <td class="text-left font-bold">Finalist</td>
+              <td class="text-left">DEFCON 26th CTF Hacking Competition World Final</td>
+              <td class="text-right italic text-red-600">Las Vegas, U.S.A</td>
             </tr>
             <tr>
-              <td>2017</td>
-              <td>Finalist</td>
-              <td>DEFCON 25th CTF Hacking Competition World Final</td>
-              <td>Las Vegas, U.S.A</td>
+              <td class="text-left">2017</td>
+              <td class="text-left font-bold">Finalist</td>
+              <td class="text-left">DEFCON 25th CTF Hacking Competition World Final</td>
+              <td class="text-right italic text-red-600">Las Vegas, U.S.A</td>
             </tr>
             <tr>
-              <td>2014</td>
-              <td>Finalist</td>
-              <td>DEFCON 22nd CTF Hacking Competition World Final</td>
-              <td>Las Vegas, U.S.A</td>
+              <td class="text-left">2014</td>
+              <td class="text-left font-bold">Finalist</td>
+              <td class="text-left">DEFCON 22nd CTF Hacking Competition World Final</td>
+              <td class="text-right italic text-red-600">Las Vegas, U.S.A</td>
             </tr>
           </tbody>
         </table>
       </div>
       <div>
         <h3 class="text-xl uppercase">Domestic</h3>
-        <table class="table-auto border-separate border-spacing-x-3">
+        <table class="table-auto w-full border-collapse">
           <tbody>
             <tr>
-              <td>2015</td>
-              <td>3rd Place</td>
-              <td>WITHCON Hacking Competition Final</td>
-              <td>Seoul, S.Korea</td>
+              <td class="text-left">2015</td>
+              <td class="text-left font-bold">3rd Place</td>
+              <td class="text-left">WITHCON Hacking Competition Final</td>
+              <td class="text-right italic text-red-600">Seoul, S.Korea</td>
             </tr>
             <tr>
-              <td>2017</td>
-              <td>Silver Prize</td>
-              <td>KISA HDCON Hacking Competition Final</td>
-              <td>Seoul, S.Korea</td>
+              <td class="text-left">2017</td>
+              <td class="text-left font-bold">Silver Prize</td>
+              <td class="text-left">KISA HDCON Hacking Competition Final</td>
+              <td class="text-right italic text-red-600">Seoul, S.Korea</td>
             </tr>
             <tr>
-              <td>2013</td>
-              <td>Silver Prize</td>
-              <td>KISA HDCON Hacking Competition Final</td>
-              <td>Seoul, S.Korea</td>
+              <td class="text-left">2013</td>
+              <td class="text-left font-bold">Silver Prize</td>
+              <td class="text-left">KISA HDCON Hacking Competition Final</td>
+              <td class="text-right italic text-red-600">Seoul, S.Korea</td>
             </tr>
           </tbody>
         </table>
