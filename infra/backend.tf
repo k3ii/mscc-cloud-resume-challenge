0cb38ae71d70ecff6e570b13142ba7df2a2891cf
k3ii <jainr@ringier.co.za>
2024-07-05 14:20:22 +0400

    Add terraform remote state

--- infra/backend.tf
+++ infra/backend.tf
@@ -7,3 +7,12 @@ terraform {
     dynamodb_table = "mscc-crc-tf-lock-table"
   }
 }
+
+data "terraform_remote_state" "state" {
+  backend = "s3"
+  config = {
+    bucket = "mscc-crc-tfstate"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
