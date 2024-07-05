14b55ebef3bb0cf946c938234efed09f517f033a
k3ii <jainr@ringier.co.za>
2024-07-05 13:48:58 +0400

    Add s3 policy

--- infra/s3.tf
+++ infra/s3.tf
@@ -29,3 +29,19 @@ resource "aws_s3_object" "crc_object" {
   key    = "index.html"
   source = "index.html"
 }
+
+resource "aws_s3_bucket_policy" "crc_bucket_policy" {
+  bucket = aws_s3_bucket.crc_bucket.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.crc_bucket.arn}/*"
+      },
+    ]
+  })
+}
