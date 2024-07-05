fa39e0defb4e9dd652533c56dc399c9b71511db0
k3ii <jainr@ringier.co.za>
2024-07-05 13:47:04 +0400

    Set S3 website

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,8 +1,31 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-test-bucket-dereck-ieee-5425984725694569"
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+resource "aws_s3_bucket" "crc_bucket" {
+  bucket        = "crc-${random_string.bucket_suffix.result}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website" "crc_bucket_website" {
+  bucket = aws_s3_bucket.crc_bucket.bucket
+  index_document = {
+    suffix = "index.html"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "crc_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.crc_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_object" "crc_object" {
+  bucket = aws_s3_bucket.crc_bucket.bucket
+  key    = "index.html"
+  source = "index.html"
 }
