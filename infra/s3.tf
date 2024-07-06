e9f669090b5c1b1a5cd6937489145f4c8139ee56
k3ii <jainr@ringier.co.za>
2024-07-06 14:00:55 +0400

    test fileset

--- infra/s3.tf
+++ infra/s3.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_public_access_block" "crc_bucket_public_access_block" {
 }
 
 resource "aws_s3_object" "crc_object" {
-  for_each = fileset(var.bucket_content, "**/*")
+  for_each = fileset("${var.bucket_content}/*", "**/*")
 
   bucket       = aws_s3_bucket.crc_bucket.bucket
   key          = each.value
@@ -27,6 +27,7 @@ resource "aws_s3_object" "crc_object" {
   content_type = each.value.content_type
 
 }
+
 
 resource "aws_s3_bucket_policy" "crc_bucket_policy" {
   bucket = aws_s3_bucket.crc_bucket.bucket
