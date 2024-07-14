39c923094035bcc48d7de026de56e64665268a9e
k3ii <k3i@protonmail.com>
2024-07-14 19:08:26 +0400

    Add global secondary index for view

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -15,6 +15,14 @@ resource "aws_dynamodb_table" "cloudresumechallenge" {
     type = "N"
   }
 
+  global_secondary_index {
+    name               = "ViewsIndex"
+    hash_key           = "views"
+    read_capacity      = 1
+    write_capacity     = 1
+    projection_type    = "ALL"
+  }
+
   tags = {
     Name        = "cloudresumechallenge-dynamodb-table"
     Environment = "production"
